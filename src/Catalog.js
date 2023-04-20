de8afd4dfc8838727f74937a1c1482dcd8474be3
crazy_lawyer <zbrzezniakan@gmail.com>
2023-04-20 16:15:42 +0200

    react3

--- src/Catalog.js
+++ src/Catalog.js
@@ -3,11 +3,13 @@ import './Catalog.css';
 
 class Catalog extends react.Component {
   render() {
+    let title = "Katalog z dnia " + new Date().toLocaleDateString();
+    let output = <><div className="Catalog"><h2>{title}</h2></div><div><b>UWAGA!</b></div></>
     return (
-      <div className="Catalog">
-        <h2>Catalog</h2>
-      </div>
+        output
     );
   }
 }
+
 export default Catalog;
+//dzieki <> </> mozemy zwracac wiecej niz jeden element

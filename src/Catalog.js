84baf3c8bd7c67be6a860e762350d864c62b7316
crazy_lawyer <zbrzezniakan@gmail.com>
2023-04-20 15:53:34 +0200

    react2

---
+++ src/Catalog.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import './Catalog.css';
+
+class Catalog extends react.Component {
+  render() {
+    return (
+      <div className="Catalog">
+        <h2>Catalog</h2>
+      </div>
+    );
+  }
+}
+export default Catalog;

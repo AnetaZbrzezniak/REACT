29de979bc1095a7ab4cfffe36f748b00a5eb5ed4
crazy_lawyer <zbrzezniakan@gmail.com>
2023-04-20 17:04:54 +0200

    reactdzis

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Catalog from './Catalog';
+import Message from './Message';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
     <Catalog/>
+    <Message/>
       </header>
     </div>
   );
--- src/Catalog.js
+++ src/Catalog.js
@@ -1,12 +1,22 @@
 import react from 'react';
 import './Catalog.css';
+import ProductList from './ProductList';
 
 class Catalog extends react.Component {
   render() {
     let title = "Katalog z dnia " + new Date().toLocaleDateString();
-    let output = <><div className="Catalog"><h2>{title}</h2></div><div><b>UWAGA!</b></div></>
     return (
-        output
+        <>
+        <div className="Catalog">
+            <h2>{title}</h2>
+                        {/* to jest komentarz */}
+            {//to jest komentarz w JSX
+            }
+            </div>
+        <div>
+            <ProductList/>
+        </div>
+        </>
     );
   }
 }
